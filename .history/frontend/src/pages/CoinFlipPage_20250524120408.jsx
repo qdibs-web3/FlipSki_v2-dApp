92c6b3cdb8b155e095d6a48914a398c331a1111b
Qdibs <72762489+qdibs@users.noreply.github.com>
2025-05-24 12:40:38 -0400

    coinUpdates

--- .history/frontend/src/pages/CoinFlipPage_20250524120408.jsx
+++ .history/frontend/src/pages/CoinFlipPage_20250524120408.jsx
@@ -14,7 +14,7 @@ import {
   baseSepoliaChain,
 } from "../config";
 import FlipSkiBaseVRFABI from "../abis/FlipSkiBaseVRF.abi.json"; 
-import coinImage from "../assets/flipski1.gif";
+import coinImage from "../assets/flipski.gif";
 import headsImage from "../assets/flip1.png";
 import tailsImage from "../assets/ski1.png";
 import "../styles/CoinFlipPage.css";
